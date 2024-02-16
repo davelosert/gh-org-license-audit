75f0e4bc8d46fe50b95c2a47d52ac8e1149d5b66
David Losert <davelosert@github.com>
2024-02-16 14:30:09 +0000

    feat: Sorts the exported licenses

--- tasks/aggregate_licenses.py
+++ tasks/aggregate_licenses.py
@@ -11,11 +11,16 @@ def aggregate_license(source_csv, target_csv):
         for row in reader:
             license = row[2]
             if license in licenses:
-                licenses[license] += int(row[3])
+                licenses[license] += 1
             else:
-                licenses[license] = int(row[3])
+                licenses[license] = 0
+
+
         with open(target_csv, "w", encoding="utf-8") as csvfile:
             writer = csv.writer(csvfile)
             writer.writerow(["License", "Count"])
+            
+            
+            licenses = dict(sorted(licenses.items(), key=lambda item: item[1], reverse=True))
             for license, count in licenses.items():
                 writer.writerow([license, str(count)])
