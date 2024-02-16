5de509e22880932f7108dbece27c14a8c410b49f
David Losert <davelosert@github.com>
2024-02-16 13:52:30 +0000

    feat: Adds binaries

---
+++ gh-org-summary.ps1
@@ -0,0 +1,10 @@
+# Get the directory of this script
+$ExtensionPath = Split-Path -parent $MyInvocation.MyCommand.Definition
+
+# if first argument is 'install'
+if ($args[0] -eq 'install') {
+    pip install ghastoolkit
+    exit
+}
+
+python3 "$ExtensionPath/main.py" $args
